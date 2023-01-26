41ef84b9cd3553bb89d08787511984b44925b2bc
Arminder Khinda <arminder.khinda@yahoo.com>
2023-01-26 13:23:47 -0800

    Add routes for admins/users and added Role middleware for authorizing specific roles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,21 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        switch(Auth::user()->role) {
+            case 'admin':
+                return redirect()->route('admin.home');
+            default:
+                return redirect()->route('user.home');
+        }
+    }
+
+    public function admin_home()
+    {
+        return view('admin/home');
+    }
+
+    public function user_home()
+    {
+        return view('user/home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +21,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// Admin Routes
+Route::prefix('admin')->middleware('role:admin')->group(function() {
+    Route::get('/home', [HomeController::class, 'admin_home'])->name('admin.home');
+});
+
+// User Routes
+Route::prefix('user')->middleware('role:user')->group(function() {
+    Route::get('/home', [HomeController::class, 'user_home'])->name('user.home');
+});
